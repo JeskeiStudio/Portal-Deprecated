0e052e69e402bc8596d8b2e44dab6007db55d360
TheNewAutonomy <darren.op@cent.finance>
2022-03-29 15:39:18 +0100

    Initial version

---
+++ JeskeiPortal/Program.cs
@@ -0,0 +1,22 @@
+using JeskeiPortal;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Nethereum.Metamask;
+using Nethereum.Metamask.Blazor;
+using Nethereum.UI;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<IMetamaskInterop, MetamaskBlazorInterop>();
+builder.Services.AddSingleton<MetamaskInterceptor>();
+builder.Services.AddSingleton<MetamaskHostProvider>();
+builder.Services.AddSingleton<IEthereumHostProvider>(serviceProvider =>
+{
+    return serviceProvider.GetService<MetamaskHostProvider>();
+});
+builder.Services.AddSingleton<NethereumSiweAuthenticatorService>();
+
+await builder.Build().RunAsync();
