667011db80eb80d67d5e7e7d29b31d858eabc01b
TheNewAutonomy <darren.op@cent.finance>
2022-08-21 00:13:22 +0100

    update assembly versions and pass through asset name

--- JeskeiPortal/Program.cs
+++ JeskeiPortal/Program.cs
@@ -7,7 +7,6 @@ using Nethereum.UI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<IMetamaskInterop, MetamaskBlazorInterop>();
